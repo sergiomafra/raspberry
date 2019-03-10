5cd8b5a3afdd600fea0dd6b83084d036724c62ed
Sérgio Mafra <sergio@mafra.io>
2019-03-09 23:27:43 -0300

    Added support for WLAN only.

--- find.sh
+++ find.sh
@@ -2,22 +2,17 @@
 
 ## VERIFY IF nmap IS INSTALLED.
 ## IF NOT, INSTALL IT.
-
 NMAP_IS_INSTALLED=$(which nmap)
 if test -z $NMAP_IS_INSTALLED
 then
 	sudo apt-get install nmap
 fi
 
-
 ## GET NETWORK IP
-
-ETH0_IP=$(ifconfig | grep -m 1 "inet" | awk '{print $2}')
-
+ETH0_OR_WLAN_IP=$(ifconfig | grep -A 1 -E "\b[^l][[:alnum:]]+: flags\b" | grep -m 1 "inet" | awk '{ print $2 }')
 
 ## LOOK FOR RASPBERRY PI
-
-NET=$(echo ${ETH0_IP} | grep -Eo "([0-9]{1,3}\.){3}")
+NET=$(echo ${ETH0_OR_WLAN_IP} | grep -Eo "([0-9]{1,3}\.){3}")
 SUBNET="${NET}0/24"
-RASP_IP=$(sudo nmap -sn $SUBNET | grep -B 2 "Raspberry Pi Foundation" | grep -Eo "([0-9]{1,3}(\.)){3}[0-9]{1,3}")
+RASP_IP=$(sudo nmap -sP $SUBNET | grep -B 2 "Raspberry Pi Foundation" | grep -Eo "([0-9]{1,3}(\.)){3}[0-9]{1,3}")
 echo $RASP_IP
